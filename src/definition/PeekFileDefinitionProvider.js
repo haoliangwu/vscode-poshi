0ff4a292d8f6ddf077aa0bca4c13c4029a280f40
haoliang.wu <haoliang.wu@liferay.com>
2016-04-24 22:09:03 +0800

    record

--- src/definition/PeekFileDefinitionProvider.js
+++ src/definition/PeekFileDefinitionProvider.js
@@ -13,9 +13,9 @@ export default class PeekFileDefinitionProvider {
     let word = document.getText(document.getWordRangeAtPosition(position))
     let line = document.lineAt(position)
 
-    console.log('====== peek-file definition lookup ===========')
-    console.log('word: ' + word)
-    console.log('line: ' + line.text)
+    // console.log('====== peek-file definition lookup ===========')
+    // console.log('word: ' + word)
+    // console.log('line: ' + line.text)
 
     // We are looking for strings with filenames
     // - simple hack for now we look for the string with our current word in it on our line
@@ -31,11 +31,11 @@ export default class PeekFileDefinitionProvider {
       let root_fname = potential_fname.split('#')[0]
       let command_name = potential_fname.split('#')[1]
       let match_start = match.index
+      let sep_index = match.index + potential_fname.indexOf('#')
       let match_end = match.index + potential_fname.length
 
       // Verify the match string is at same location as cursor
-      if ((position.character >= match_start) &&
-        (position.character <= match_end)) {
+      if (position.character >= match_start && position.character <= match_end) {
         let full_path = path.resolve(working_dir, `${root_fname}.${type}`)
 
         console.log(' Match: ', match)
@@ -47,7 +47,19 @@ export default class PeekFileDefinitionProvider {
         if (fs.existsSync(full_path)) {
           return workspace.openTextDocument(full_path)
             .then(doc => {
-              return new Location(Uri.file(full_path), new Position(0, 1))
+              if (position.character <= sep_index) {
+                return new Location(Uri.file(full_path), new Position(0, 1))
+              } else {
+                const lines = doc.getText().split(/\r?\n/g)
+                for (let i = 0; i < lines.length; i++) {
+                  const index = lines[i].indexOf(`name="${command_name}"`)
+
+                  if (index > 0) {
+                    return new Location(Uri.file(full_path), new Position(i, index))
+                  }
+                }
+                return new Location(Uri.file(full_path), new Position(0, 1))
+              }
             })
         }
       }
